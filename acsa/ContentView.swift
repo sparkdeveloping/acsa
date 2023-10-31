4262ac06b9f3295a0ca5ee6c8d4e9d2ba30d2b08
Denzel <sbj.developing@gmail.com>
2023-10-31 10:37:40 -0500

    Initial Commit

---
+++ acsa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  acsa
+//
+//  Created by Denzel Nyatsanza on 10/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ acsa/acsaApp.swift
@@ -0,0 +1,17 @@
+//
+//  acsaApp.swift
+//  acsa
+//
+//  Created by Denzel Nyatsanza on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct acsaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
