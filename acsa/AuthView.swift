e5b4f7f644182a4c060b79730b478363c6ce091e
Denzel <sbj.developing@gmail.com>
2023-11-03 17:00:01 -0500

    first commit

---
+++ acsa/AuthView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthView.swift
+//  acsa
+//
+//  Created by Denzel Nyatsanza on 10/31/23.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthView()
+}
